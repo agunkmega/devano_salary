395ea3f69c3964bf603e542e2e1606d8ffa531c5
Admin Devano <admin@devanosilver.com>
2026-06-03 10:47:28 +0800

    fix: enlarge logo on login page h-16->h-28, no bg container

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,7 +20,7 @@
                 <a href="/">
                     @php $logoPath = \App\Models\Setting::where('key', 'app_logo')?->value('value'); @endphp
                     @if($logoPath)
-                    <img src="{{ \Illuminate\Support\Facades\Storage::url($logoPath) }}" alt="Logo" class="h-16 w-auto">
+                    <img src="{{ \Illuminate\Support\Facades\Storage::url($logoPath) }}" alt="Logo" class="h-28 w-auto">
                     @else
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500 dark:text-gray-400" />
                     @endif

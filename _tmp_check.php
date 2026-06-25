3aaa3e9ab6c48a12a6413a98fa422eec8dafbf62
Admin Devano <admin@devanosilver.com>
2026-06-25 13:35:07 +0800

    chore: remove temp file

--- _tmp_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require __DIR__ . '/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-foreach (App\Models\Shift::all() as $s) {
-    echo $s->id . ' | ' . $s->name . PHP_EOL;
-}
-unlink(__FILE__);
